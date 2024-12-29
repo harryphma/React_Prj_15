951b21b1a6a779d9faf1a241b3def306120c1892
harryphma <phamquanghung2705@gmail.com>
2024-12-28 19:43:15 -0700

    add real time location

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import Places from './Places.jsx';
 import { useState, useEffect } from 'react';
-import ErrorPage from './Error.jsx';
+import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 
 
@@ -20,15 +21,21 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
         if (!response.ok){
           throw new Error('Failed to fetch places');
-          
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude);
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+
+        
       } catch (error){
         setError({message: error.message || 'Could not fetch places. Try again'});
+        setIsFetching(false);
       }
       
-      setIsFetching(false);
+      
     }
 
     fetchPlaces();
@@ -36,7 +43,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
   if (error){
     return (
-      <ErrorPage title='An error occured!' message={error.message}></ErrorPage>
+      <Error title='An error occured!' message={error.message}></Error>
     );
   }
 
