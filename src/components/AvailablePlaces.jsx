0a6e766af5b311d723fb4e1abfa658b60930a1ef
harryphma <phamquanghung2705@gmail.com>
2024-12-28 18:51:23 -0700

    start fetching

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,26 @@
 import Places from './Places.jsx';
+import { useState } from 'react';
+
+
 
 export default function AvailablePlaces({ onSelectPlace }) {
+
+
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  fetch('http://localhost:3000/places')
+    .then((response) => {
+      return response.json();
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places);
+    });
+
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
